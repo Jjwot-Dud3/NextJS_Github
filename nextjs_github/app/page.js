c54cb8353783351d55413cde40a5a1ab03196084
Jjwot-Dud3 <1085855@est.intec.edu.do>
2023-08-21 17:00:41 -0400

    Agregando logica test para consumir api de github y provider de github para hacer log in

--- nextjs_github/app/page.js
+++ nextjs_github/app/page.js
@@ -1,16 +1,27 @@
 import Image from 'next/image'
 import LoginComponent from '../components/logincomponent';
-/*
-const octokit = new Octokit({
-  auth: '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'
-})
-var data = await octokit.request('GET /users', {
-  headers: {
-    'X-GitHub-Api-Version': '2022-11-28'
-  }
-})*/
+import { Octokit } from 'octokit';
+
+
 //console.log(data)
 export default function Home() {
+  async function fectchGitHubUser (){
+    const octokit = new Octokit({
+      auth: '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'
+    })
+    var data = await octokit.request('GET /users/{username}', {
+      username:'Jjwot-Dud3',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+
+    }) 
+    return (data)
+}
+  fectchGitHubUser().then((respon) =>{
+    console.log(respon)
+  });
+  
   return (
     <div>
       <LoginComponent />

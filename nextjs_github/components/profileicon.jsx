e812d3779f4f6cafb3d6ebf66bab94901acee4d2
Jjwot-Dud3 <1085855@est.intec.edu.do>
2023-08-22 00:12:49 -0400

    adding title to image in profileicon

--- nextjs_github/components/profileicon.jsx
+++ nextjs_github/components/profileicon.jsx
@@ -8,7 +8,7 @@ export default function ProfileIcon(){
     if (status === "authenticated"){
         return (
             <div className="flex gap-4">
-                <Image src={session.user.image} width={40} height={40} className="rounded-full"/>
+                <Image src={session.user.image} width={40} height={40} className="rounded-full" title={session.user.name}/>
                 <button className="font-bold" onClick={() => signOut()}> Sign Out</button>
             </div>
         );

23ddabbb8d6bc4303e98a5f3b4287465d413e5d0
Jjwot-Dud3 <1085855@est.intec.edu.do>
2023-08-22 00:08:50 -0400

    Changing visuals and deleting comments for logging component and navbar

--- nextjs_github/components/logincomponent.jsx
+++ nextjs_github/components/logincomponent.jsx
@@ -1,10 +1,9 @@
 "use client";
-import React , { useState }from 'react';
+import React from 'react';
 import {signIn, useSession} from "next-auth/react";
 
 const LoginComponent = () => {
-  // const [username, setUsername] = useState('');
-  // const [password, setPassword] = useState('');
+
   const data = useSession();
   // const handleCopy = (event) => {
   //   event.preventDefault(); // Prevent default copy behavior
@@ -15,23 +14,7 @@ const LoginComponent = () => {
       <div className="bg-white p-8 rounded shadow-md w-64">
       
         <h2 className="text-xl font-semibold mb-4 text-black">Login with GitHub</h2>
-        {/* <div className="mb-4">
-          <input
-            type="text"
-            placeholder="Username"
-            className="w-full px-3 py-2 border rounded text-black"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full px-3 py-2 border rounded text-black"
-            onChange={(e) => setPassword(e.target.value)}
-            onCopy={handleCopy}
-          />
-        </div> */}
+        
         <button className="w-full bg-blue-500 text-white py-2 rounded" onClick={()=> signIn("github")}>
           Login
         </button>
--- nextjs_github/components/profileicon.jsx
+++ nextjs_github/components/profileicon.jsx
@@ -1,13 +1,15 @@
 "use client";
 
 import {signIn, useSession, signOut} from "next-auth/react";
+import Image from "next/image";
 
 export default function ProfileIcon(){
     const{data: session, status} = useSession();
     if (status === "authenticated"){
         return (
-            <div>
-                <button onClick={() => signOut()}> Sign Out</button>
+            <div className="flex gap-4">
+                <Image src={session.user.image} width={40} height={40} className="rounded-full"/>
+                <button className="font-bold" onClick={() => signOut()}> Sign Out</button>
             </div>
         );
     }

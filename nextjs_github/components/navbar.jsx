f40a5552517ca66ddde01ddc6ebc5040803d74de
Jjwot-Dud3 <1085855@est.intec.edu.do>
2023-08-22 00:09:48 -0400

    Changing text color for navbar

--- nextjs_github/components/navbar.jsx
+++ nextjs_github/components/navbar.jsx
@@ -3,7 +3,7 @@ import ProfileIcon from "./profileicon";
 
 export default function NavBar(){
     return(
-        <nav className="flex items-center justify-between fixed left-0 right-0 px-8 h-14 bg-purple-600 text-black">
+        <nav className="flex items-center justify-between fixed left-0 right-0 px-8 h-14 bg-purple-600 text-white">
             <Link classname="font-bold " href={"/"}>NextJS_Github</Link>
             <ProfileIcon/>
         </nav>

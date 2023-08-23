4ce2f903089385009072a74a022d094c76ae541d
Jjwot-Dud3 <1085855@est.intec.edu.do>
2023-08-23 12:42:32 -0400

    Adding behavior that when press favorite button it disables the button and shows a message

--- nextjs_github/app/components/repositorydetails.jsx
+++ nextjs_github/app/components/repositorydetails.jsx
@@ -6,63 +6,89 @@ import {FaPlus} from 'react-icons/fa';
 import PocketBase from 'pocketbase';
 
 
-export default function RepositoryDetails({params}) {
-    const pb = new PocketBase('http://127.0.0.1:8090')
-    const  owner  = params.owner
-    const repoName = params.reponame
+export default function RepositoryDetails({ params }) {
+    const pb = new PocketBase('http://127.0.0.1:8090');
+    const owner = params.owner;
+    const repoName = params.reponame;
     const [repoData, setRepoData] = useState(null);
-    console.log(owner)
-    console.log(repoName)
-
+    const [successMessage, setSuccessMessage] = useState(null);
+    const [isRepoFavorite, setIsRepoFavorite] = useState(false);
+  
+    console.log(owner);
+    console.log(repoName);
+  
     useEffect(() => {
+      async function fetchRepoData() {
         const octokit = new Octokit({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET, 
-            
+          clientId: process.env.GITHUB_CLIENT_ID,
+          clientSecret: process.env.GITHUB_CLIENT_SECRET,
         });
-    
-        octokit.request('GET /users/{owner}/repos',{
+  
+        try {
+          const response = await octokit.request('GET /users/{owner}/repos', {
             owner: owner,
             headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-            }
-        })
-        .then(response => {
-        setRepoData(response.data.find(repo => repo.name === repoName));
-        console.log(response.data.find(repo => repo.name === repoName))
-        })
-        .catch(error => {
-        console.error('Error fetching repository data:', error);
-        });
+              'X-GitHub-Api-Version': '2022-11-28',
+            },
+          });
+  
+          const matchingRepo = response.data.find((repo) => repo.name === repoName);
+          setRepoData(matchingRepo);
+          setIsRepoFavorite(await isFavorite(matchingRepo.id));
+        } catch (error) {
+          console.error('Error fetching repository data:', error);
+        }
+      }
+  
+      fetchRepoData();
     }, [owner, repoName]);
-
-    const handleAddToFavorites = async function () {
-            console.log("handleAddToFavorites");
-            const data = {
-                "repo_id": repoData.id,
-                "owner": owner,
-                "name": repoData.name,
-                "url": repoData.html_url,
-                "private": repoData.private,
-                "created_at": repoData.created_at,
-                "update_at": repoData.updated_at,
-                "homepage": repoData.homepage
-            }
-            console.log(data)
-            const record = await pb.collection('Repositories').create(data);
+  
+    const handleAddToFavorites = async () => {
+      const data = {
+        repo_id: repoData.id,
+        owner: owner,
+        name: repoData.name,
+        url: repoData.html_url,
+        private: repoData.private,
+        created_at: repoData.created_at,
+        update_at: repoData.updated_at,
+        homepage: repoData.homepage,
+      };
+  
+      try {
+        await pb.collection('Repositories').create(data);
+        setSuccessMessage('Repository added to favorites.');
+        setIsRepoFavorite(true);
+      } catch (error) {
+        console.error('Error adding repository to favorites:', error);
+      }
     };
-
+  
+    const isFavorite = async (repoId) => {
+      const resultList = await pb.collection('Repositories').getList(1, 50, {
+        filter: `repo_id = ${repoId}`,
+      });
+  
+      return resultList.length > 0;
+    };
+  
     return (
-        <div className="p-6 grid grid-cols-2 gap-4">
+      <div className="p-6 grid grid-cols-2 gap-4">
         {repoData && (
-            <div className="bg-white p-4 rounded shadow-md ">
-                <div className="flex items-center justify-between">
-                    <h3 className="text-lg font-semibold mb-2 text-black">Repository Details</h3>
-                    <button className="flex bg-blue-500 text-white py-2 px-4 rounded items-center gap-2" onClick={handleAddToFavorites}>
-                        <FaPlus/>Add to Favorites
-                    </button>
-                </div>
-                <p className="text-black">Owner: {owner}</p>
+          <div className="bg-white p-4 rounded shadow-md">
+            <div className="flex items-center justify-between">
+              <h3 className="text-lg font-semibold mb-2 text-black">Repository Details</h3>
+              <button
+                className={`flex ${
+                  isRepoFavorite ? 'bg-gray-400' : 'bg-blue-500'
+                } text-white py-2 px-4 rounded items-center gap-2`}
+                onClick={isRepoFavorite ? null : handleAddToFavorites}
+                disabled={isRepoFavorite}
+              >
+                <FaPlus /> Add to Favorites
+              </button>
+            </div>
+            <p className="text-black">Owner: {owner}</p>
                 <p className="text-black">Repository Name: {repoData.name}</p>
                 <p className="text-black">Repository Id: {repoData.id}</p>
                 <p className="text-black">Repository Url: {repoData.html_url}</p>
@@ -70,8 +96,11 @@ export default function RepositoryDetails({params}) {
                 <p className="text-black">Created: {repoData.created_at.split("T")[0]}</p>
                 <p className="text-black">Last Updated: {repoData.updated_at.split("T")[0]}</p>
                 <p className="text-black">Homepage: {repoData.homepage?? "N/A"}</p>
-            </div>
+          </div>
         )}
-        </div>
+        {successMessage && (
+          <p className="text-green-500 mt-4">{successMessage}</p>
+        )}
+      </div>
     );
-};
+  }

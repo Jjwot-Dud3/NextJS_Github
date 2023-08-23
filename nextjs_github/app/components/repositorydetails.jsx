684e129baadd69ecf5b41714346ab0d9d304efd6
Jjwot-Dud3 <1085855@est.intec.edu.do>
2023-08-23 09:00:42 -0400

    Adding buttom to add favorites to repository details and added package react-icons

--- nextjs_github/app/components/repositorydetails.jsx
+++ nextjs_github/app/components/repositorydetails.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { Octokit } from 'octokit';
+import {FaPlus} from 'react-icons/fa'
 
 export default function RepositoryDetails({params}) {
   const  owner  = params.owner
@@ -32,19 +33,28 @@ export default function RepositoryDetails({params}) {
     });
   }, [owner, repoName]);
 
+  const handleAddToFavorites = function () {
+        console.log("handleAddToFavorites");
+  };
+
   return (
-    <div className="p-6">
+    <div className="p-6 grid grid-cols-2 gap-4">
       {repoData && (
-        <div className="bg-white p-4 rounded shadow-md">
-          <h3 className="text-lg font-semibold mb-2 text-black">Repository Details</h3>
-          <p className="text-black">Owner: {owner}</p>
-          <p className="text-black">Repository Name: {repoData.name}</p>
-          <p className="text-black">Repository Id: {repoData.id}</p>
-          <p className="text-black">Repository Url: {repoData.html_url}</p>
-          <p className="text-black">Is Private?: {repoData.private== false ? "False": "True"}</p>
-          <p className="text-black">Created: {repoData.created_at.split("T")[0]}</p>
-          <p className="text-black">Last Updated: {repoData.updated_at.split("T")[0]}</p>
-          <p className="text-black">Homepage: {repoData.homepage}</p>
+        <div className="bg-white p-4 rounded shadow-md ">
+            <div className="flex items-center justify-between">
+                <h3 className="text-lg font-semibold mb-2 text-black">Repository Details</h3>
+                <button className="flex bg-blue-500 text-white py-2 px-4 rounded items-center gap-2" onClick={handleAddToFavorites}>
+                    <FaPlus/>Add to Favorites
+                </button>
+            </div>
+            <p className="text-black">Owner: {owner}</p>
+            <p className="text-black">Repository Name: {repoData.name}</p>
+            <p className="text-black">Repository Id: {repoData.id}</p>
+            <p className="text-black">Repository Url: {repoData.html_url}</p>
+            <p className="text-black">Is Private?: {repoData.private== false ? "False": "True"}</p>
+            <p className="text-black">Created: {repoData.created_at.split("T")[0]}</p>
+            <p className="text-black">Last Updated: {repoData.updated_at.split("T")[0]}</p>
+            <p className="text-black">Homepage: {repoData.homepage?? "N/A"}</p>
         </div>
       )}
     </div>
